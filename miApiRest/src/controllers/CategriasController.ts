c6e465eea88ce6db7f3c425c23c8983b8f98aeb2
DenBrais <denisbrais2005@gmail.com>
2026-02-03 14:50:38 -0600

    transitory commit

--- miApiRest/src/controllers/CategriasController.ts
+++ miApiRest/src/controllers/CategriasController.ts
@@ -2,9 +2,36 @@ import { Request, Response } from "express";
 
 class CategoriasController {
   // Métodos del controlador de categorías
+
+  //metodo Create
+  static createCategories = async (req: Request, res: Response) => {
+    // Lógica para crear una nueva categoría
+    res.status(201).json({ message: "Categoría creada exitosamente" });
+  };
+  //metodo read all
   static getAllCategories = async (req: Request, res: Response) => {
     // Lógica para obtener todas las categorías
     res.status(200).json({ message: "Obtener todas las categorías" });
+  };
+
+  //metodo read one
+  static getCategoryById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // Lógica para obtener una categoría por su ID
+    res.status(200).json({ message: `Obtener categoría con ID: ${id}` });
+  };
+  //metodo update
+  static updateCategories = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // Lógica para actualizar una categoría por su ID
+    res.status(200).json({ message: `Actualizar categoría con ID: ${id}` });
+  };
+
+  //metodo delete
+  static deleteCategories = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    // Lógica para eliminar una categoría por su ID
+    res.status(200).json({ message: `Eliminar categoría con ID: ${id}` });
   };
 }
 export default CategoriasController;
